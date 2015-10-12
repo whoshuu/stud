c0022aa34581f7bbc9ba628b4aef21cf39225903
Huu Nguyen <whoshuu@gmail.com>
2015-10-12 13:22:22 -0400

    Add rest of std::string constructors

--- include/stud/string.h
+++ include/stud/string.h
@@ -1,6 +1,7 @@
 #ifndef STUD_String_H
 #define STUD_String_H
 
+#include <initializer_list>
 #include <string>
 
 
@@ -14,7 +15,16 @@ class string : public std::string {
     string& operator=(const string& rhs) = default;
     string& operator=(string&& rhs) = default;
     explicit string(const char* raw_string) : std::string(raw_string) {}
+    explicit string(const char* raw_string, size_t length) : std::string(raw_string, length) {}
+    explicit string(size_t to_fill, char character) : std::string(to_fill, character) {}
     explicit string(const std::string& std_string) : std::string(std_string) {}
+    explicit string(const std::string& std_string, size_t position,
+                    size_t length = std::string::npos)
+            : std::string(std_string, position, length) {}
+    explicit string(std::initializer_list<char> il) : std::string(il) {}
+    template <class InputIterator>
+    explicit string(InputIterator first, InputIterator last)
+            : std::string(first, last) {}
 };
 
 } // namespace stud
